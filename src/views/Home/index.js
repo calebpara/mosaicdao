c7a6ae53e85ec218db6c671dd36cf6f3e5834cec
susanreele <susanreele23@gmail.com>
2021-11-17 15:31:08 -0600

    first commit, structure setup

---
+++ src/views/Home/index.js
@@ -0,0 +1,15 @@
+
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Home
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
